7ddc4f21efe958e7d6295fd8bcf5a32d33cfaf1c
Cornel Stoica <cornel.stoica@oit.edu>
2023-11-14 01:32:36 -0800

    Emptied gitignore.

---
+++ node_modules/express_server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 5523;
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+/* CURRENT ENDPOINTS 'local_state' or 'queries' */
+app.use("/api/local_state", require("./routes/api/local_state"));
+app.use("/api/queries", require("./routes/api/queries"));
+
+// Catch all for page request errors
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "Peugeot404.jpg"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}`);
+});
+
+module.exports = app
